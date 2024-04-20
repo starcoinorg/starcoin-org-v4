1516c2031d361c8023a457152eb87da1bffeb19c
airen <airenliao@gmail.com>
2024-04-20 21:01:52 +0800

    调整导航菜单模板

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,21 +1,17 @@
-import { useState, type MouseEvent } from 'react';
-import clsx from 'clsx';
-import I18n, { type LocaleCode } from '@/i18n';
+import { useState, type MouseEvent } from "react";
+import clsx from "clsx";
+import I18n, { type LocaleCode } from "@/i18n";
 
-import nav_data, {type NavGroup} from './nav-data';
+import nav_data, { type NavGroup } from "./nav-data";
 
-export default function Navigation({
-  locale
-}: {
-  locale: LocaleCode
-}) {
-  const t = I18n.create_trans(locale)
-  const [active_group, set_active_group] = useState(nav_data[0].group)
+export default function Navigation({ locale }: { locale: LocaleCode }) {
+  const t = I18n.create_trans(locale);
+  const [active_group, set_active_group] = useState(nav_data[0].group);
 
   function change_active_group(evt: MouseEvent, ng: NavGroup) {
-    evt.preventDefault()
+    evt.preventDefault();
     if (ng.group !== active_group) {
-      set_active_group(ng.group) 
+      set_active_group(ng.group);
     }
   }
 
@@ -24,27 +20,31 @@ export default function Navigation({
       {nav_data.map((o) => {
         return (
           <li className="dropdown" key={o.group}>
-            <a href={o.href} className={clsx('dropdown__toggle', { active: o.group === active_group })}>
-              {t(o.t_key)}
-              <svg className="icon--arrow-right" onClick={(evt) => change_active_group(evt, o)}>
+            <span
+              className={clsx("dropdown__toggle", {
+                active: o.group === active_group,
+              })}
+            >
+              <a href={o.href}>{t(o.t_key)}</a>
+              <svg
+                className="icon--arrow-right"
+                onClick={(evt) => change_active_group(evt, o)}
+              >
                 <use xlinkHref="#arrow-right" href="#arrow-right" />
-                </svg>
-            </a>
+              </svg>
+            </span>
             <ul className="dropdown__content">
               {o.links.map((l) => {
                 return (
                   <li key={l.t_key}>
-                    <a href={l.href}>
-                      {t(l.t_key)}
-                    </a>
+                    <a href={l.href}>{t(l.t_key)}</a>
                   </li>
-                )
+                );
               })}
             </ul>
           </li>
-        )
+        );
       })}
     </ul>
   );
 }
-
