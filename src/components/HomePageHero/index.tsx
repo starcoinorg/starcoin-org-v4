9950c2f06ac343cca20712acc35bedca8beb0651
airen <airenliao@gmail.com>
2024-04-26 18:01:22 +0800

    update style

--- src/components/HomePageHero/index.tsx
+++ src/components/HomePageHero/index.tsx
@@ -12,8 +12,8 @@ export default function HomePageHero({ locale }: { locale: LocaleCode }) {
     <section className="hero hero--home">
       <div className="hero__container section__container">
         <h2 className="hero__title">
-          <p> {t("home.hero.title")} </p>
-          <p dangerouslySetInnerHTML={{ __html: t("home.hero.subtitle") }} />
+          <div> {t("home.hero.title")} </div>
+          <div dangerouslySetInnerHTML={{ __html: t("home.hero.subtitle") }} />
         </h2>
         <div className="hero__content">
           <p dangerouslySetInnerHTML={{ __html: t("home.hero.desc_p1") }} />
